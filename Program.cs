1688d8fb3902ccb5f1b45b662d6453d958d7521e
Tony Rdz <TonyRuco@gmail.com>
2021-05-21 14:07:12 -0500

    add api access token scope

--- Program.cs
+++ Program.cs
@@ -24,6 +24,7 @@ namespace RefreshTokenExpSample
                 builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
                 options.ProviderOptions.DefaultAccessTokenScopes.Add("openid");
                 options.ProviderOptions.DefaultAccessTokenScopes.Add("offline_access");
+                options.ProviderOptions.DefaultAccessTokenScopes.Add("https://ExWAMDigitalB2C.onmicrosoft.com/fcbec44b-355b-44a4-8287-a47a77ef9984/QAiApi.All");
             });
 
             await builder.Build().RunAsync();
